a4aa63874e749975f21ed6e47a9741c3ab038328
Hao Nguyen <nxhao235@gmail.com>
2023-09-25 14:00:42 +0700

    Update authorization example

--- newsmanagement/newsmanagement/schemas/custom_schemas.py
+++ newsmanagement/newsmanagement/schemas/custom_schemas.py
@@ -1,11 +1,12 @@
 from drf_spectacular.openapi import AutoSchema
-from drf_spectacular.utils import OpenApiParameter
+from drf_spectacular.utils import OpenApiParameter, OpenApiExample
 
 
 class CustomAutoSchema(AutoSchema):
     global_params = [
-        OpenApiParameter(name="Authorization", type=str,
-                         location=OpenApiParameter.HEADER, description="Authorization token")
+        OpenApiParameter(name="Authorization", type=str, required=True, examples=[
+            OpenApiExample('Example 1', value='Token 401f7ac837da42b97f613d789819ff93537bee6a')
+        ], location=OpenApiParameter.HEADER, description="Authorization token")
     ]
 
     def get_override_parameters(self):
